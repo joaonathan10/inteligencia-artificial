7957220e073d8e15934243896be391c160f343ee
joaonathan10 <111013836+joaonathan10@users.noreply.github.com>
2024-09-12 08:31:24 -0300

    1209

--- aleatorios.js
+++ aleatorios.js
@@ -1,4 +1,4 @@
-const nomes = ["Ana", "Fernanda", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Nathan", "Jean Malesza"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()*lista.length);   
